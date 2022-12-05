a1a71b92a6ccf7183e85d8cf4e83e76bc041bf92
Aaron Ehresman <aaronehresman@Aarons-MacBook-Pro.local>
2022-12-04 22:43:53 -0800

    clean up

--- day5/day5.js
+++ day5/day5.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 fs.readFile("input.txt", "utf8", (err, data) => {
   if (err) throw err;
 
+  //test data
   const testInput = [
     "move 1 from 2 to 1",
     "move 3 from 1 to 3",
@@ -11,64 +12,64 @@ fs.readFile("input.txt", "utf8", (err, data) => {
   ];
   const testStacks = ["NZ", "DCM", "P"];
 
+  //real data
   const input = data.split("\n").splice(10);
-  part1(stacks, input);
+  const stacks = [
+    "CQB",
+    "ZWQR",
+    "VLRMB",
+    "WTVHZC",
+    "GVNBHZD",
+    "QVFJCPNH",
+    "SZWRTGD",
+    "PZWBNMGC",
+    "PFQWMBJN",
+  ];
 
-  console.log("\x1b[94m Part 1 Answer: \x1b[0m", part1(stacks, input));
-  //console.log("\x1b[96m Part 2 Answer: \x1b[0m", part2(data));
+  console.log(
+    "\x1b[94m Part 1 Answer: \x1b[0m",
+    part1and2(stacks, input, partId.PART_ONE)
+  );
+  console.log(
+    "\x1b[96m Part 2 Answer: \x1b[0m",
+    part1and2(stacks, input, partId.PART_TWO)
+  );
 });
 
-const stacks = [
-  "CQB",
-  "ZWQR",
-  "VLRMB",
-  "WTVHZC",
-  "GVNBHZD",
-  "QVFJCPNH",
-  "SZWRTGD",
-  "PZWBNMGC",
-  "PFQWMBJN",
-];
+const partId = {
+  PART_ONE: "1",
+  PART_TWO: "2",
+};
 
-const moveCrate = (string, stackArrays) => {
+const moveCrate = (string, stackArrays, partId) => {
   const stringAsArray = string.split(" ");
   const numberToMove = parseInt(stringAsArray[1]);
   const moveFrom = parseInt(stringAsArray[3]);
   const moveTo = parseInt(stringAsArray[5]);
 
-  for (let i = 1; i <= numberToMove; i++) {
-    stackArrays[moveTo - 1].unshift(stackArrays[moveFrom - 1].shift());
+  if (partId === "1") {
+    for (let i = 1; i <= numberToMove; i++) {
+      stackArrays[moveTo - 1].unshift(stackArrays[moveFrom - 1].shift());
+    }
+  } else if (partId === "2") {
+    stackArrays[moveTo - 1].unshift(
+      ...stackArrays[moveFrom - 1].splice(0, numberToMove)
+    );
   }
 };
 
-const moveCrate9001 = (string, stackArrays) => {
-  const stringAsArray = string.split(" ");
-  const numberToMove = parseInt(stringAsArray[1]);
-  const moveFrom = parseInt(stringAsArray[3]);
-  const moveTo = parseInt(stringAsArray[5]);
-
-  stackArrays[moveTo - 1].unshift(
-    ...stackArrays[moveFrom - 1].splice(0, numberToMove)
-  );
-};
-
-const part1 = (stacks, input) => {
+const part1and2 = (stacks, input, partId) => {
   let topCrates = "";
   const stackArrays = stacks.map((e) => e.split(""));
   for (const instruction of input) {
-    //part 1
-    moveCrate(instruction, stackArrays);
-    //part 2
-    //moveCrate9001(instruction, stackArrays);
+    //part 1 (CrateMover 9000)
+    partId === "1"
+      ? moveCrate(instruction, [...stackArrays], "1")
+      : //part 2 (CrateMover 9001)
+        moveCrate(instruction, [...stackArrays], "2");
   }
-  //return data;
-
   for (const stack of stackArrays) {
     topCrates += stack[0];
   }
   return topCrates;
-};
-
-const part2 = (data) => {
-  return data;
 };
